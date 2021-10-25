6bc4bdba41dcac5f59a6bd82fd766f26e8942603
Jc Cometa <cometburn@gmail.com>
2021-10-26 05:20:34 +0800

    Edit

--- config/config.js
+++ config/config.js
@@ -1,40 +1,43 @@
-require('dotenv').config(); // this is important!
+require('dotenv').config();
+// this is important!
 module.exports = {
-	"development": {
-		"username": process.env.DB_USER,
-		"password": process.env.DB_PASSWORD,
-		"database": process.env.DB_NAME,
-		"host": process.env.DB_HOST,
-		"dialect": "postgres",
-		"logging": false,
-		dialectOptions: {
-				useUTC: false
-		},
-		define: {
-				schema: "public"
-		}
-	},
-	"test": {
-		"username": process.env.DB_USER,
-		"password": process.env.DB_PASSWORD,
-		"database": process.env.DB_NAME,
-		"host": process.env.DB_HOST,
-		"dialect": "postgres",
-		dialectOptions: {
-				useUTC: false
-		},
-	},
-	"production": {
-		"username": process.env.DB_USER,
-		"password": process.env.DB_PASSWORD,
-		"database": process.env.DB_NAME,
-		"host": process.env.DB_HOST,
-		"dialect": "postgres",
-		dialectOptions: {
-				useUTC: false
-		},
-		define: {
-				schema: "public"
-		}
-	}
-}
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+    logging: false,
+    dialectOptions: {
+      useUTC: false,
+    },
+    define: {
+      schema: 'public',
+    },
+  },
+  test: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+    dialectOptions: {
+      useUTC: false,
+    },
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+    logging: false,
+    rejectUnauthorized: false,
+    dialectOptions: {
+      useUTC: false,
+    },
+    define: {
+      schema: 'public',
+    },
+  },
+};
